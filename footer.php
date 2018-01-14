3d2e8f2e61dffe233c5385fe09e482b63b8011f9
kok3114 <kokchang0519@gmail.com>
2018-01-15 03:51:49 +0800

    footer file added

---
+++ footer.php
@@ -0,0 +1,16 @@
+ Volume in drive C is Windows
+ Volume Serial Number is 5C36-B875
+
+ Directory of C:\Users\kokch\Desktop\Projects\Java
+
+01/15/2018  03:51 AM    <DIR>          .
+01/15/2018  03:51 AM    <DIR>          ..
+01/15/2018  02:56 AM               606 body.php
+01/15/2018  03:51 AM                 0 footer.php
+01/15/2018  03:14 AM               674 Hello.class
+01/14/2018  11:40 PM               155 Hello.java
+01/15/2018  01:28 AM               557 index.html
+01/15/2018  01:27 AM               506 readme.txt
+01/15/2018  12:27 AM    <DIR>          src
+               6 File(s)          2,498 bytes
+               3 Dir(s)  859,904,761,856 bytes free
